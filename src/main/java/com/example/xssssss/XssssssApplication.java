c8c12d0c97d8beb7903a47f2b348966a789cf163
Jorge Guillen Rivera <jorgegrr052002@gmail.com>
2022-09-06 11:29:21 -0500

    h

---
+++ src/main/java/com/example/xssssss/XssssssApplication.java
@@ -0,0 +1,13 @@
+package com.example.xssssss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XssssssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XssssssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xssssss/XssssssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xssssss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XssssssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
